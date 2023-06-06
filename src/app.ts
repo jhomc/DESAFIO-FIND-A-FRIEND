d8bca586cbdcf883bf50c5feb1ad9b9914ef0c9b
Jhonata Costa <jhomcosta@gmail.com>
2023-06-06 14:04:32 -0300

    chore: jwt setup

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,29 @@
 import fastify from 'fastify'
 import { organizationsRoutes } from './http/controllers/organizations/routes'
+import fastifyJwt from '@fastify/jwt'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
+
 app.register(organizationsRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.log(error)
+  } else {
+    // TODO: external tool -> Datadog/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error' })
+})
