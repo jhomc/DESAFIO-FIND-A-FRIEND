879d2728e665f1a294b3dc278a107ebda572ae39
Jhonata Costa <jhomcosta@gmail.com>
2023-06-06 10:12:53 -0300

    test: create organization test implemented

---
+++ src/repositories/in-memory/in-memory-organizations-repository.ts
@@ -0,0 +1,27 @@
+import { Organization, Prisma } from '@prisma/client'
+import { OrganizationsRepository } from '../organizations-repository'
+import { randomUUID } from 'crypto'
+
+export class InMemoryOrganizationsRepository
+  implements OrganizationsRepository
+{
+  public items: Organization[] = []
+
+  async create(data: Prisma.OrganizationUncheckedCreateInput) {
+    const organization = {
+      id: randomUUID(),
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      phone: data.phone,
+      street: data.street,
+      address_number: data.address_number,
+      cep: data.cep,
+      created_at: new Date(),
+    }
+
+    this.items.push(organization)
+
+    return organization
+  }
+}
---
+++ src/use-case/create-organization.spec.ts
@@ -0,0 +1,27 @@
+import { InMemoryOrganizationsRepository } from '@/repositories/in-memory/in-memory-organizations-repository'
+import { CreateOrganizationUseCase } from './create-organization-use-case'
+import { beforeEach, describe, expect, it } from 'vitest'
+
+let organizationsRepository: InMemoryOrganizationsRepository
+let sut: CreateOrganizationUseCase
+
+describe('Create Organization Use Case', () => {
+  beforeEach(() => {
+    organizationsRepository = new InMemoryOrganizationsRepository()
+    sut = new CreateOrganizationUseCase(organizationsRepository)
+  })
+
+  it('should be able to create organization', async () => {
+    const { organization } = await sut.execute({
+      name: 'Jhon Org',
+      email: 'jhon.org@gmail.com',
+      password: '123456',
+      street: 'Test street',
+      phone: '41996196231',
+      cep: 81650220,
+      address_number: '312, house 5',
+    })
+
+    expect(organization.id).toEqual(expect.any(String))
+  })
+})
